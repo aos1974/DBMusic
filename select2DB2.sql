e3ca132c47dd54c7f5d7e9bdb9042dbee2f55c63
Aleksandr Sysoev <aosysoev@yandex.ru>
2022-06-08 22:54:32 +0300

    Corrected tasks

--- select2DB2.sql
+++ select2DB2.sql
@@ -4,10 +4,10 @@ JOIN genre_list gl ON g.id = gl.genre_id
 GROUP BY name;
 
 -- количество треков, вошедших в альбомы 2000-2020 годов --
-SELECT COUNT(id) FROM songs s 
-WHERE album_id IN (
-	SELECT id FROM albums a 
-	WHERE releasedate BETWEEN '01/01/2000' AND '31/12/2020');
+SELECT COUNT(s.id) FROM albums a
+JOIN songs s ON a.id = s.album_id 
+WHERE a.releasedate BETWEEN '01/01/2000' AND '31/12/2020'
+;
 
 -- средняя продолжительность треков по каждому альбому --
 SELECT a.name, AVG(duration) FROM albums a 
@@ -16,42 +16,37 @@ GROUP BY a.name
 ;
 
 -- все исполнители, которые не выпустили альбомы в 2020 году --
-SELECT sl.name FROM author_list al 
+SELECT DISTINCT sl.name FROM author_list al 
 JOIN singer_list sl ON al.singer_id = sl.id 
 WHERE album_id NOT IN (
 	SELECT id FROM albums a 
 	WHERE releasedate BETWEEN '01/01/2020' AND '31/12/2020')
-GROUP BY sl.name;
+;
 
 -- названия сборников, в которых присутствует конкретный исполнитель: 'Michael Jackson' --
-SELECT c.name FROM collections c 
+SELECT DISTINCT c.name FROM collections c 
 JOIN collection_list cl ON c.id = cl.collection_id 
 JOIN songs s ON s.id = cl.song_id 
 JOIN albums a ON a.id = s.album_id 
 JOIN author_list al ON al.album_id = a.id 
 JOIN singer_list sl ON sl.id = al.singer_id 
 WHERE sl.name LIKE 'Michael Jackson'
-GROUP BY c.name
 ;
 
 -- название альбомов, в которых присутствуют исполнители более 1 жанра --
 SELECT a.name FROM albums a 
 JOIN author_list al ON al.album_id = a.id 
 JOIN singer_list sl ON sl.id = al.singer_id 
-WHERE sl.id IN (
-	SELECT sl.id FROM singer_list sl 
-	JOIN genre_list gl ON gl.singer_id = sl.id 
-	JOIN genres g ON g.id = gl.genre_id 
-	GROUP BY sl.id, sl.name
-	HAVING COUNT(sl.id) > 1
-);
+JOIN genre_list gl ON gl.singer_id = sl.id 
+GROUP BY a.name
+HAVING COUNT (gl.singer_id) > 1
+;
 
 -- наименование треков, которые не входят в сборники --
 SELECT s.name FROM songs s 
-WHERE s.id NOT IN (
-	SELECT song_id FROM collection_list cl 
-	GROUP BY song_id
-);
+LEFT JOIN collection_list cl ON s.id = cl.song_id 
+WHERE cl.collection_id IS NULL
+;
 
 -- исполнителя(-ей), написавшего самый короткий по продолжительности трек --
 SELECT sl.name, s.name AS song_name, s.duration FROM songs s 
@@ -60,7 +55,6 @@ JOIN author_list al ON al.album_id = a.id
 JOIN singer_list sl ON sl.id = al.singer_id
 WHERE s.duration = (
 	SELECT MIN(duration) FROM songs)
-GROUP BY s.duration, s.name, s.id, sl.name
 ORDER BY s.duration 
 ;
 
